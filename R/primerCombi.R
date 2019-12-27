cd4a60712b1918a3eb14adbbfb332ce0a448d201
Sizhong Yang <noasark@163.com>
2019-12-27 22:23:06 +0100

    Update primerCombi.R

--- R/primerCombi.R
+++ R/primerCombi.R
@@ -1,7 +1,4 @@
 primerCombi <- function(primerfile){
-  ## df must contains two columns with colnames of Forward, reverse, or for-rev,
-  # forward = primerfile[, grep("for",colnames(primerfile), ignore.case = TRUE)]
-  # reverse = primerfile[, grep("rev",colnames(primerfile), ignore.case = TRUE)]
   forward = primerfile[, 1]
   reverse = primerfile[, 2]
   comb = expand.grid(forward,reverse)
