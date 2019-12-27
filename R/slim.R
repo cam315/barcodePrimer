aebc23a521319b39563ce2ea9bb1eefeeeca94cf
Sizhong Yang <noasark@163.com>
2019-12-27 22:24:22 +0100

    Update slim.R

--- R/slim.R
+++ R/slim.R
@@ -1,8 +1,7 @@
 slim <- function(x,y){
-  ## This is a key function to filter wrong combine from theoretical combination
   if(any(sapply(x,is.factor))) x[] = lapply(x, as.character)
   if(any(sapply(y,is.factor))) y[] = lapply(y, as.character)
-  x = x[do.call(order, x),] # This allows to sort whole df based on whole columns, but without specify colunn names
+  x = x[do.call(order, x),] 
   y = y[do.call(order, y),]
   result = x[!duplicated(rbind(y, x))[nrow(y) + 1:nrow(x)], ]
   return(result)
